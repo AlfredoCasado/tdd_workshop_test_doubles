8dbc7d468d2c8bb1a9f3b2eb83f899e5d1ce669e
Alfredo Casado <casado.alfredo@unience.com>
2013-11-05 11:35:09 +0100

    refactor: remove duplication in test

--- solutions/alfredo-groovy/src/test/groovy/tdd/marsrover/MarsRoverControlUnitSpec.groovy
+++ solutions/alfredo-groovy/src/test/groovy/tdd/marsrover/MarsRoverControlUnitSpec.groovy
@@ -4,28 +4,18 @@ import spock.lang.*
 
 class MarsRoverControlUnitSpec extends Specification {
 
+	def engineSpy = Mock(Engine)
+	def marsRover = new MarsRoverControlUnit(engine: engineSpy)
+
+
 	def "can move fordward once"() {
-		given:
-			def engineSpy = Mock(Engine)
-			def marsRover = new MarsRoverControlUnit(engine: engineSpy)
-
-		when: 
-			marsRover.move("f")
-
-		then: 
-			1 * engineSpy.fordward(1)
+		when: marsRover.move("f")
+		then: 1 * engineSpy.fordward(1)
 	}
 
 	def "can move backward once"() {
-		given: 
-			def engineSpy = Mock(Engine)
-			def marsRover = new MarsRoverControlUnit(engine: engineSpy)
-
-		when:
-			marsRover.move("b")
-
-		then:
-			1 * engineSpy.backward(1)
+		when: marsRover.move("b")
+		then: 1 * engineSpy.backward(1)
 	}
 
 }
